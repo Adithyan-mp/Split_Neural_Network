88dce0db5c7a2dd511d407f6f2a558911209f5d5
Appu <adithyanmp2806@gmail.com>
2025-03-09 01:29:46 +0530

    fedavg

---
+++ FedAvg/client3.py
@@ -0,0 +1,152 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader
+import torch.distributed as dist
+from datetime import timedelta
+import torch.nn.functional as F
+import numpy as np
+import os
+import logging
+import random
+
+# logging.basicConfig(filename='/app/smashed.log', level=logging.INFO,format='%(asctime)s %(message)s')
+
+# random seed settings
+random.seed(43)
+np.random.seed(43)
+torch.manual_seed(43)
+
+#connection
+def init(rank, world_size, backend='gloo'):
+    os.environ['GLOO_SOCKET_IFNAME'] = 'eth0'
+    os.environ['MASTER_ADDR'] = 'client1'
+    os.environ['MASTER_PORT'] = '29500'
+
+    dist.init_process_group(
+        backend=backend,
+        world_size=world_size,
+        rank=rank,
+        timeout=timedelta(seconds=180),
+    )
+
+    print(f"Rank {rank}: is_initialized and ready to communicate...")
+    return dist.is_initialized()
+
+def recv(arr,src):
+    dist.recv(tensor=arr, src=src)
+
+def send(arr,dst):
+    dist.send(tensor=arr, dst=dst)
+
+def terminate(rank):
+    dist.destroy_process_group()
+    print(f"{rank} succesfully terminated...")
+    
+def send_model(model, dst):
+    for key, param in model.state_dict().items():
+        dist.send(param, dst=dst)
+        print(f"Sent {key}")
+    
+
+def recv_model(model, src):
+    for key, param in model.state_dict().items():
+        temp_tensor = torch.empty_like(param)  
+        dist.recv(temp_tensor, src=src)  
+        param.data.copy_(temp_tensor)  
+        print(f"Received {key}, shape: {temp_tensor.shape}")
+
+
+#loading dataset
+class TrainDataset(Dataset):
+    def __init__(self,transform=None,path_x='/app/X_train.npy',path_y='/app/y_train.npy'):
+        self.x= np.load(path_x)
+        self.y = np.load(path_y)
+        self.n = self.x.shape[0]
+        self.height = self.x.shape[1]
+        self.width = self.x.shape[2]
+        self.transform =transform
+        
+    def __getitem__(self, index):
+        sample = self.x[index],self.y[index]
+        
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+    
+    def __len__(self):
+        return self.n
+    
+    def shape(self):
+        return self.n,self.height,self.width
+    
+
+#transform
+class ToTensor:
+    def __call__(self,sample):
+        input,target=sample
+        input = input.reshape((1,28,28)).astype('float32')
+        return torch.from_numpy(input),target
+
+#model
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN,self).__init__()
+        self.conv1 = nn.Conv2d(1,10,kernel_size=5)
+        self.conv2 = nn.Conv2d(10,20,kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1=nn.Linear(320,50)
+        self.fc2=nn.Linear(50,10)
+    
+    def forward(self,x):
+        x = F.relu(F.max_pool2d(self.conv1(x),2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)),2))
+        x = x.view(-1,320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x,training=self.training)
+        x = self.fc2(x)
+        return x
+
+#batch and loss
+batch_size = 250
+transform = ToTensor()
+dataset = TrainDataset(path_x='/fed/x_fed_3.npy',path_y='/fed/y_fed_3.npy',transform=transform)
+dataloader = DataLoader(dataset=dataset,shuffle=False,batch_size=batch_size)
+
+model = CNN()
+criterion=nn.CrossEntropyLoss()
+optim = torch.optim.SGD(model.parameters(), lr=0.005)
+
+#training loop
+def run(num_epoch,round,curr_round, model=model, optim=optim,
+        dataloader=dataloader, 
+        criterion=criterion):
+    recv_model(model=model,src=3)
+    
+    for epoch in range(num_epoch):
+        for i, (feature,target) in enumerate(dataloader):
+            
+            logits = model(feature)
+            loss = criterion(logits,target.view(-1))
+            
+            loss.backward()
+            
+            optim.step()
+            optim.zero_grad()
+            
+            print(f"round : {curr_round}/{round} epoch : {epoch}/{num_epoch} {i}/{len(dataloader)} loss : {loss}")
+    
+    send_model(model=model,dst=3)
+
+world_size = 4
+rank = 2
+num_epoch = 10
+batch_size = 250
+round = 5
+n=torch.tensor([len(dataset)])
+print(n.size())
+init(rank=rank,world_size=world_size)
+send(arr=n,dst=3)
+for i in range(round):
+    run(num_epoch,round=round,curr_round=i)
+
+terminate(rank=rank)
---
+++ FedAvg/server.py
@@ -0,0 +1,165 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader
+import torch.distributed as dist
+from datetime import timedelta
+import torch.nn.functional as F
+import numpy as np
+import os
+import logging
+import random
+
+logging.basicConfig(filename='/app/smashed.log', level=logging.INFO,format='%(asctime)s %(message)s')
+
+# random seed settings
+random.seed(43)
+np.random.seed(43)
+torch.manual_seed(43)
+
+#connection
+def init(rank, world_size, backend='gloo'):
+    os.environ['GLOO_SOCKET_IFNAME'] = 'eth0'
+    os.environ['MASTER_ADDR'] = 'client1'
+    os.environ['MASTER_PORT'] = '29500'
+
+    dist.init_process_group(
+        backend=backend,
+        world_size=world_size,
+        rank=rank,
+        timeout=timedelta(seconds=180),
+    )
+
+    print(f"Rank {rank}: is_initialized and ready to communicate...")
+    return dist.is_initialized()
+
+def recv(arr,src):
+    dist.recv(tensor=arr, src=src)
+
+def send(arr,dst):
+    dist.send(tensor=arr, dst=dst)
+
+def terminate(rank):
+    dist.destroy_process_group()
+    print(f"{rank} succesfully terminated...")
+    
+def send_model(model, dst):
+    for key, param in model.state_dict().items():
+        dist.send(param, dst=dst)
+        print(f"Sent {key}")
+    
+def recv_model(model, src):
+    for key, param in model.state_dict().items():
+        temp_tensor = torch.empty_like(param)  
+        dist.recv(temp_tensor, src=src)  
+        param.data.copy_(temp_tensor)  
+        print(f"Received {key}, shape: {temp_tensor.shape}")
+        
+def recv_model_update(model, src, n, m):
+    for key, param in model.state_dict().items():
+        temp_tensor = torch.empty_like(param)  
+        dist.recv(temp_tensor, src=src)
+        avg_tensor = torch.zeros_like(param)
+        if src == 0:
+            avg_tensor = (temp_tensor * (n[0,src] / m))  
+        else:
+            avg_tensor = param.clone()
+            avg_tensor += (temp_tensor * (n[0,src] / m)) 
+        
+        param.data.copy_(avg_tensor.clone())  
+        print(f"Received {key}, shape: {avg_tensor.shape}")
+
+
+
+#loading dataset
+class TrainDataset(Dataset):
+    def __init__(self,transform=None,path_x='/app/X_train.npy',path_y='/app/y_train.npy'):
+        self.x= np.load(path_x)
+        self.y = np.load(path_y)
+        self.n = self.x.shape[0]
+        self.height = self.x.shape[1]
+        self.width = self.x.shape[2]
+        self.transform =transform
+        
+    def __getitem__(self, index):
+        sample = self.x[index],self.y[index]
+        
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+    
+    def __len__(self):
+        return self.n
+    
+    def shape(self):
+        return self.n,self.height,self.width
+    
+
+#transform
+class ToTensor:
+    def __call__(self,sample):
+        input,target=sample
+        input = input.reshape((1,28,28)).astype('float32')
+        return torch.from_numpy(input),target
+
+#model
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN,self).__init__()
+        self.conv1 = nn.Conv2d(1,10,kernel_size=5)
+        self.conv2 = nn.Conv2d(10,20,kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1=nn.Linear(320,50)
+        self.fc2=nn.Linear(50,10)
+    
+    def forward(self,x):
+        x = F.relu(F.max_pool2d(self.conv1(x),2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)),2))
+        x = x.view(-1,320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x,training=self.training)
+        x = self.fc2(x)
+        return x
+
+#batch and loss
+# batch_size = 250
+# transform = ToTensor()
+# dataset = TrainDataset(path_x='/app/X_train.npy',path_y='/app/y_train.npy',transform=transform)
+# dataloader = DataLoader(dataset=dataset,shuffle=False,batch_size=batch_size)
+
+model = CNN()
+# criterion=nn.CrossEntropyLoss()
+# optim = torch.optim.SGD(model.parameters(), lr=0.005)
+
+def recv_data(N):
+    n = torch.zeros(size=(1,N-1))
+    for i in range(N-1):
+        temp = torch.tensor([0])
+        print(temp.size())
+        recv(temp,src = i)
+        n[0,i] = temp.item()
+    m = n.sum()
+    return n,m
+    
+    
+
+#training loop
+def run(num_epoch,round,n,m,N, model=model):
+    for i in range(N-1):
+        send_model(model=model,dst=i)
+        
+    for i in range(N-1):
+        recv_model_update(model=model,src=i,n=n,m=m)
+
+
+
+world_size = 4
+rank = 3
+num_epoch = 10
+batch_size = 250
+round = 5
+init(rank=rank,world_size=world_size)
+n,m = recv_data(N=world_size)
+for i in range(round):
+    run(num_epoch=num_epoch,round=round,n=n,m=m,N=world_size)
+
+terminate(rank=rank)
